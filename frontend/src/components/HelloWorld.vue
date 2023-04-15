cd3e5f48915abcd35ef8747b2e3d341c188ccacb
LeoonWi <redwind090@gmail.com>
2023-04-15 16:36:53 +0300

    few settings

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -39,7 +39,7 @@ export default {
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
+<!-- Добавьте атрибут "scoped", чтобы ограничить CSS только этим компонентом -->
 <style scoped>
 h3 {
   margin: 40px 0 0;
